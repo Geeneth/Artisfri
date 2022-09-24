c007c311f0f23a8f1343371dcb825bd9efb61b32
Geeneth Kulatunge <kulatungegeeneth@gmail.com>
2022-09-24 01:38:14 -0400

    Updated Database
    Implemented search functionality to database page. Added navigation bar to the database page.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom
 import HomePage from "./home-page";
 import Database from "./database";
 
+//temp
+import DatabaseTrial from "./database";
+
 function App() {
   return (
     //Using react router to navigate between pages
@@ -14,7 +17,7 @@ function App() {
       <div className="App" id="app">
         <Routes>
         <Route path="/" element={<HomePage />}/>
-        <Route path="/database" element={<Database />}/>
+        <Route path="/database" element={<DatabaseTrial />}/>
         </Routes>
       </div>
     </Router>
--- src/database.js
+++ src/database.js
@@ -4,29 +4,81 @@ import AssetVerticalCard from "./components/asset-vertical-card";
 import { products } from "./components/database-data";
 import { arrivals } from "./components/new-arrivals";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useState } from "react";
 
 function Database() {
+  const [query, setQuery] = useState("");
   return (
     <div id="database-page">
-      <h3 className="return-home">
-        <Link id="database-return" to="/">
-          Return to home page
-        </Link>
-      </h3>
+      <div className="database-nav-bar">
+        <nav className="database-nav">
+          {/* <h2 className="full-name">ARTISFRI</h2> */}
+          <h2 className="return-home">
+            <Link id="database-return" to="/">
+              ARTISFRI
+            </Link>
+          </h2>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {/* <li>
+              <a href="#about-us">About Us</a>
+            </li>
+            <li>
+              <a href="#team-section">Team</a>
+            </li>
+            <li>
+              <a href="#socials">Socials</a>
+            </li> */}
+          </ul>
+        </nav>
+      </div>
+      <div id="search-area">
+        <div className="search-bar-container">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value.toLowerCase())}
+          />
+        </div>
 
-      <div className="database-products">
+        {/* ______________________________________________________________ */}
+        <div className="database-content-area">
+          {/* categories temp */}
 
-        {/* Mapping through the array of products and creating cards for each*/}
-        {products.map((product, index) => (
-          <div className="vertical-asset-card-block">
-            <AssetVerticalCard
-              title={product.title}
-              description={product.description}
-              image={product.image}
-              links={product.links}
-            />
-          </div>
-        ))}
+          {/* <div className="categories-list">
+            <h2 className="categories-title">Categories</h2>
+            <ul>
+              <li>Art</li>
+              <li>Music</li>
+              <li>Photography</li>
+              <li>Writing</li>
+              <li>Design</li>
+              <li>Programming</li>
+              <li>Other</li>
+            </ul>
+          </div> */}
+          {/* ______________________________________________________________ */}
+
+          <ul className="list">
+            {products
+              .filter((products) =>
+                products.title.toLowerCase().includes(query)
+              )
+              .map((product, index) => (
+                <div className="vertical-asset-card-block">
+                  <AssetVerticalCard
+                    title={product.title}
+                    description={product.description}
+                    image={product.image}
+                    links={product.links}
+                  />
+                </div>
+              ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
