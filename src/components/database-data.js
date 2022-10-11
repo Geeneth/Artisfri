b4caa69e884a873918bc3d732641677adbaa4a5a
Geeneth Kulatunge <kulatungegeeneth@gmail.com>
2022-10-11 02:25:02 -0400

    Added Categories
    Added the ability for the user to filter the models by their category.

--- src/components/database-data.js
+++ src/components/database-data.js
@@ -2,96 +2,98 @@
 
 export const products = [
   {
+    category:"Furniture",
     title: "Table",
-    description:
-      "This is a great table to add to your collection. It can serve very well in creating a set of a room.",
+    description:"This is a great table to add to your collection. It can serve very well in creating a set of a room.",
     image: "./assets/amantha/images/Table_Furniture.jpg",
     links: ["./assets/amantha/Table_Furniture.fbx"],
   },
   {
+    category:"Furniture",
     title: "TV Stand",
-    description:
-      "A TV Stand is a great piece of furniture to add to your 3D home environment. It can be used to create a set of a room.",
+    description:"A TV Stand is a great piece of furniture to add to your 3D home environment. It can be used to create a set of a room.",
     image: "./assets/amantha/images/TV_Stand.png",
     links: ["./assets/amantha/TV_Stand_Furniture.fbx"],
   },
   {
+    category:"Furniture",
     title: "Wooden Chair",
     description: "A basic wooden chair to add to your 3D environment. ",
     image: "./assets/amantha/images/Wooden_Chair_Furniture.png",
     links: ["./assets/amantha/Wooden_Chair_Furniture.fbx"],
   },
   {
+    category:"Furniture",
     title: "Chair",
-    description:
-      "A basic chair to add to your 3D environment.",
+    description:"A basic chair to add to your 3D environment.",
     image: "./assets/amantha/images/Chair_Furniture.png",
     links: ["./assets/amantha/Chair_Furniture.fbx"],
   },
   {
+    category:"Environment",
     title: "Basic Tree",
-    description:
-      "A very basic tree to add to your 3D environment. Works well with other low poly assets.",
+    description:"A very basic tree to add to your 3D environment. Works well with other low poly assets.",
     image: "./assets/dimantha/images/tree-1.png",
     links: ["./assets/dimantha/tree-1.fbx"],
   },
   {
+    category:"Environment",
     title: "Basic Tree 2",
-    description:
-      "A very basic tree to add to your 3D environment. Works well with other low poly assets.",
+    description:"A very basic tree to add to your 3D environment. Works well with other low poly assets.",
     image: "./assets/dimantha/images/tree-2.png",
     links: ["./assets/dimantha/tree-2.fbx"],
   },
   {
+    category:"Environment",
     title: "Basic Tree 3",
-    description:
-      "A very basic tree to add to your 3D environment. Works well with other low poly assets.",
+    description:"A very basic tree to add to your 3D environment. Works well with other low poly assets.",
     image: "./assets/dimantha/images/tree-3.png",
     links: ["./assets/dimantha/tree-3.fbx"],
   },
   {
+    category:"Environment",
     title: "Withered Tree",
-    description:
-      "A very basic tree to add to your 3D environment. Works well with other low poly assets.",
+    description:"A very basic tree to add to your 3D environment. Works well with other low poly assets.",
     image: "./assets/dimantha/images/withered-tree-1.png",
     links: ["./assets/dimantha/withered-tree-1.fbx"],
   },
   {
+    category:"Environment",
     title: "Withered Tree 2",
-    description:
-      "A very basic tree to add to your 3D environment. Works well with other low poly assets.",
+    description:"A very basic tree to add to your 3D environment. Works well with other low poly assets.",
     image: "./assets/dimantha/images/withered-tree-2.png",
     links: ["./assets/dimantha/withered-tree-2.fbx"],
   },
   {
+    category:"Environment",
     title: "Large Grass",
-    description:
-      "A very basic model of grass to add to your 3D environment. Works well with other low poly assets.",
+    description:"A very basic model of grass to add to your 3D environment. Works well with other low poly assets.",
     image: "./assets/dimantha/images/grass-large.png",
     links: ["./assets/dimantha/grass-large.fbx"],
   },
   {
+    category:"Environment",
     title: "Medium Grass",
-    description:
-    "A very basic model of grass to add to your 3D environment. Works well with other low poly assets.",
+    description:"A very basic model of grass to add to your 3D environment. Works well with other low poly assets.",
     image: "./assets/dimantha/images/grass-medium.png",
     links: ["./assets/dimantha/grass-medium.fbx"],
   },
   {
+    category:"Environment",
     title: "Small Grass",
-    description:
-    "A very basic model of grass to add to your 3D environment. Works well with other low poly assets.",
+    description:"A very basic model of grass to add to your 3D environment. Works well with other low poly assets.",
     image: "./assets/dimantha/images/grass-small.png",
     links: ["./assets/dimantha/grass-small.fbx"],
   },
   {
+    category:"Environment",
     title: "Pair of Rocks",
-    description:
-      "A pair of low poly rocks to add to your 3D environment. Works well with other low poly assets.",
+    description:"A pair of low poly rocks to add to your 3D environment. Works well with other low poly assets.",
     image: "./assets/dimantha/images/rocks.png",
     links: ["./assets/dimantha/rocks.fbx"],
   },
 ];
+
 
 export default {
   products,
--- src/database.js
+++ src/database.js
@@ -7,7 +7,32 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { useState } from "react";
 
 function Database() {
+  const [data, setData] = useState(products);
   const [query, setQuery] = useState("");
+
+  const filter = (button, e) => {
+    //making the colors of the buttons uniform
+    let elements = document.getElementsByClassName("btn");
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.backgroundColor = "#ffc371";
+      elements[i].style.color = "black";
+    }
+    //changing the color of the button that was clicked
+    document.getElementById(button).style.backgroundColor = "#000";
+    document.getElementById(button).style.color = "#FFF";
+    if (button === "All") {
+      //if the button clicked is "All", then show all the data
+      setData(products);
+    } else {
+      //if the button clicked is not "All", then filter the data
+      const filtered = products.filter((sorted) => {
+        return sorted.category === button;
+      });
+      //set the data to the filtered data
+      console.log(filtered);
+      setData(filtered);
+    }
+  };
   return (
     <div id="database-page">
       <div className="database-nav-bar">
@@ -35,41 +60,55 @@ function Database() {
           />
         </div>
 
-        {/* ______________________________________________________________ */}
-        <div className="database-content-area">
-          {/* categories temp */}
-
-          {/* <div className="categories-list">
-            <h2 className="categories-title">Categories</h2>
-            <ul>
-              <li>Art</li>
-              <li>Music</li>
-              <li>Photography</li>
-              <li>Writing</li>
-              <li>Design</li>
-              <li>Programming</li>
-              <li>Other</li>
-            </ul>
-          </div> */}
-          {/* ______________________________________________________________ */}
-
-          <ul className="list">
-            {products
-              .filter((products) =>
-                products.title.toLowerCase().includes(query)
-              )
-              .map((product, index) => (
-                <div className="vertical-asset-card-block">
-                  <AssetVerticalCard
-                    title={product.title}
-                    description={product.description}
-                    image={product.image}
-                    links={product.links}
-                  />
-                </div>
-              ))}
-          </ul>
-        </div>
+        {/* table to store categories section and the models */}
+        <table className="database-table-view">
+          <tr>
+            <td className="categories">
+              <div className="categories-list">
+                {/* creating a button for each category */}
+                {/* each button uses the "filter" method defined at the top */}
+                <button id="All" className="btn" onClick={() => filter("All")}>
+                  All
+                </button>
+                <button
+                  id="Furniture"
+                  className="btn"
+                  onClick={() => filter("Furniture")}
+                >
+                  Furniture
+                </button>
+                <button
+                  id="Environment"
+                  className="btn"
+                  onClick={() => filter("Environment")}
+                >
+                  Environment
+                </button>
+              </div>
+            </td>
+            <td className="model-list">
+              <div className="list">
+                {data
+                  //filtering the data based on the query from the search bar
+                  .filter((products) =>
+                    products.title.toLowerCase().includes(query)
+                    
+                  )
+                  .map((product, index) => (
+                    <div className="vertical-asset-card-block">
+                      <AssetVerticalCard
+                        title={product.title}
+                        description={product.description}
+                        image={product.image}
+                        links={product.links}
+                      />
+                    </div>
+                  ))}
+                  
+              </div>
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
   );
